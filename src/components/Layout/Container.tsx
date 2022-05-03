54b844e3df2eafdf3b4cc8543d5d1b02d85fd4e5
Cesar Gallardo <wuelo789@gmail.com>
2022-05-02 21:40:08 -0500

    Initial commit

---
+++ src/components/Layout/Container.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+interface Props {
+  className?: string;
+  children?: ReactNode;
+}
+
+const Container = ({ className, children }: Props) => {
+  return (
+    <div
+      className={`max-w-full p-[0.5rem] md:p-0 md:max-w-[800px] lg:max-w-[960px] mx-auto ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
