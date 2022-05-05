aac3e1f9560916af77e05a1beb2685781da37997
Cesar Gallardo <wuelo789@gmail.com>
2022-05-05 13:29:51 -0500

    Added different server base url depending on the development enviroment.

--- src/Constants/Server.ts
+++ src/Constants/Server.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = 'https://bugtracker-api-version1.herokuapp.com';
+export const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://bugtracker-api-version1.herokuapp.com'
+    : 'https://localhost:44362';
