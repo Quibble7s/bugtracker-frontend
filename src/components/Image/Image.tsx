0f034eed50661c477679cea0ebc018c008dc2ce7
Cesar Gallardo <wuelo789@gmail.com>
2022-05-05 13:27:17 -0500

    Added image component

---
+++ src/components/Image/Image.tsx
@@ -0,0 +1,48 @@
+interface Props {
+  /**
+   * The source of the image.
+   */
+  src: string;
+  /**
+   * The width of the image.
+   */
+  width: number;
+  /**
+   * The height of the image.
+   */
+  height: number;
+  /**
+   * The text that the image will show if not loading correctly.
+   */
+  alt?: string;
+  /**
+   * The style of the image.
+   * @default ''
+   */
+  className?: string;
+  /**
+   * How the image will fill inside the container.
+   * @default 'fill'
+   */
+  objectFit?: 'contain' | 'cover' | 'fill' | 'scale-down' | 'none';
+}
+
+export const Image = ({
+  src,
+  width,
+  height,
+  alt,
+  className = '',
+  objectFit = 'fill',
+}: Props) => {
+  return (
+    <img
+      className={className}
+      style={{ objectFit: objectFit }}
+      src={src}
+      alt={alt}
+      width={`${width}px`}
+      height={`${height}px`}
+    />
+  );
+};
