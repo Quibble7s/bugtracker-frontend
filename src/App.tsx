cf3a06231abf9339a6e5c0133f23c4730760017d
Cesar Gallardo <wuelo789@gmail.com>
2022-05-07 19:34:07 -0500

    Added loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
+import { PageLoadingAnimation } from './Components/Animations';
 import { Navbar } from './Components/Navigation';
+import { useAuth } from './Hooks';
 import { HomePage } from './Pages/HomePage';
 
 function App() {
+  const auth = useAuth();
+  const loading: boolean =
+    localStorage.getItem('authenticated') === 'true' && auth.user === null;
+
+  if (loading) {
+    return <PageLoadingAnimation />;
+  }
+
   return (
     <>
       <header>
