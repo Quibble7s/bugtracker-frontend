0d0fe219deb78e8b982df48f86e5b1b7c2341ad9
Cesar Gallardo <wuelo789@gmail.com>
2022-05-09 23:19:23 -0500

    Added button type

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -6,13 +6,22 @@ interface Props {
   theme: ButtonTheme;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
   children: ReactNode;
+  type?: 'button' | 'reset' | 'submit';
+  className?: string;
 }
 
-export const Button = ({ theme, onClick, children }: Props) => {
+export const Button = ({
+  theme,
+  onClick,
+  children,
+  type = 'button',
+  className = '',
+}: Props) => {
   return (
     <button
+      type={type}
       onClick={onClick}
-      className={`p-[10px] rounded-md transition-colors duration-200 ${Themes[theme]}`}>
+      className={`p-[10px] rounded-md transition-colors duration-200 ${Themes[theme]} ${className}`}>
       <PS>{children}</PS>
     </button>
   );
