92e044c41e4ed0f403dff113508b80ea70874160
Cesar Gallardo <wuelo789@gmail.com>
2022-05-09 23:17:57 -0500

    Hide links and buttons on specific locations

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,10 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Container } from '../Layout';
 import { Image } from '../Image';
 import { NavigationLinks } from '.';
 import { LoginOrRegisterButtons, MobileMenu } from 'src/Sections';
 
 export const Navbar = () => {
+  const navHiddenLocations: string[] = ['/auth', '/dashboard'];
+  const location = useLocation();
+
+  const isLocationHidden = () => {
+    for (let i = 0; i < navHiddenLocations.length; i++) {
+      const hiddenLocation = navHiddenLocations[i];
+      if (location.pathname.includes(hiddenLocation)) return true;
+    }
+    return false;
+  };
+
+  const isHidden = isLocationHidden();
+
   return (
     <nav className='shadow-sm w-full absolute left-0 top-0 z-10'>
       <Container className='flex flex-row justify-between items-center'>
@@ -16,8 +29,10 @@ export const Navbar = () => {
             className='py-[15px]'
           />
         </Link>
-        <NavigationLinks className='hidden lg:flex' />
-        <LoginOrRegisterButtons className='hidden lg:flex flex-row gap-3 items-center' />
+        {!isHidden && <NavigationLinks className='hidden lg:flex' />}
+        {!isHidden && (
+          <LoginOrRegisterButtons className='hidden lg:flex flex-row gap-3 items-center' />
+        )}
         <MobileMenu />
       </Container>
     </nav>
