69328dd91ecb99bcda6e1c5f7ff7034d307e5cb4
Cesar Gallardo <wuelo789@gmail.com>
2022-05-12 19:56:26 -0500

    Added location state

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,12 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Container } from '../Layout';
 import { Image } from '../Image';
 import { NavigationLinks } from '.';
 import { LoginOrRegisterButtons, MobileMenu, NavDropDown } from 'src/Sections';
+import { MutableRefObject, useRef } from 'react';
 
 export const Navbar = () => {
+  const location = useLocation();
+  const navRef: MutableRefObject<HTMLElement> = useRef(null!);
+  const state = location.pathname.includes('/auth') ? 'disabled' : 'active';
+  const themes = {
+    active: 'fixed bg-light-blue',
+    disabled: 'absolute',
+  };
+
   return (
-    <nav className=' w-full absolute left-0 top-0 z-10'>
+    <nav ref={navRef} className={`w-full left-0 top-0 z-10 ${themes[state]}`}>
       <Container className='flex flex-row justify-between items-center'>
         <Link to='/'>
           <Image
