8b8cc192db473770789cebd7d9b87075493919b4
Cesar Gallardo <wuelo789@gmail.com>
2022-05-12 19:58:02 -0500

    Changed background behavior

--- src/Sections/AuthSection.tsx
+++ src/Sections/AuthSection.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { Image } from 'src/Components/Image';
 import { Container } from 'src/Components/Layout';
 import { H1, H2 } from 'src/Components/Typography';
 
@@ -14,15 +15,29 @@ export const AuthSection = ({
   return (
     <main className='relative overflow-x-hidden h-fit overflow-y-hidden'>
       <div className='absolute bottom-0 left-0 -translate-x-1/2 translate-y-1/2 w-[300px] md:w-[500px] lg:w-[800px] h-auto z-[-1] opacity-[0.4]'>
-        <img src='/static/images/background.png' alt=' ' />
+        <Image
+          src='/static/images/background.png'
+          width={800}
+          height={800}
+          onLoad={(e) => {
+            e.currentTarget.classList.add('fade-in');
+          }}
+        />
       </div>
       <div className='absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 w-[300px] md:w-[500px] lg:w-[800px] h-auto z-[-1] opacity-[0.4]'>
-        <img src='/static/images/background.png' alt=' ' />
+        <Image
+          src='/static/images/background.png'
+          width={800}
+          height={800}
+          onLoad={(e) => {
+            e.currentTarget.classList.add('fade-in');
+          }}
+        />
       </div>
-      <Container className='min-h-screen flex flex-col justify-center items-center'>
+      <Container className='my-[40px] min-h-screen flex flex-col justify-center items-center lg:my-0'>
         <div className='grid grid-cols-1 lg:grid-cols-2'>
           <div>
-            <H1 className='text-center mt-20 md:mt-0 fade-in lg:text-left'>
+            <H1 className='text-center md:mt-0 fade-in lg:text-left'>
               {title}
             </H1>
             <H2 className='text-themeGray mix-blend-multiply font-normal mt-8 text-center fade-in lg:text-left'>
