74b1be3a32147b432a0e26999dd4309627765ee7
Cesar Gallardo <wuelo789@gmail.com>
2022-05-15 10:10:11 -0500

    Changed the layout

--- src/Sections/HomePage/Footer.tsx
+++ src/Sections/HomePage/Footer.tsx
@@ -9,23 +9,23 @@ export const Footer = () => {
     <footer className='mt-64'>
       <Container>
         <div className='border-b border-themeGray py-4 flex flex-row justify-between items-center'>
-          <div className='flex flex-row gap-4 items-center'>
+          <div className='flex flex-col-reverse w-full gap-4 justify-between items-center lg:flex-row'>
             <div className='relative'>
               <span className='block absolute top-0 right-0 bottom-0 left-0 bg-themeBlack mix-blend-color' />
               <Image src='/static/images/logo.svg' width={140} height={30} />
             </div>
-            <div className='flex flex-row gap-4'>
+            <div className='flex flex-col items-center gap-4 lg:flex-row'>
               {NavLinks.map((link) => (
                 <NavigationLink to={link.to}>{link.label}</NavigationLink>
               ))}
             </div>
           </div>
         </div>
-        <div className='py-4 flex flex-row items-center justify-between'>
+        <div className='py-4 flex flex-row items-center justify-center'>
           <PXS className='text-center'>
-            &copy; bugtracker - {new Date().getFullYear()}
+            &copy; bugtracker - {new Date().getFullYear()} |{' '}
+            <span className='text-red-500'>this is a demo website</span>
           </PXS>
-          <PXS className='text-red-500 text-center'>THIS IS A DEMO WEBSITE</PXS>
         </div>
       </Container>
     </footer>
