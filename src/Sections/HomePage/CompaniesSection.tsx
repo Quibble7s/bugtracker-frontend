9f1ee0faf1e509dc9f18fec9b508950fb1f5c4da
Cesar Gallardo <wuelo789@gmail.com>
2022-05-15 13:35:31 -0500

    Changed style

--- src/Sections/HomePage/CompaniesSection.tsx
+++ src/Sections/HomePage/CompaniesSection.tsx
@@ -7,14 +7,10 @@ export const CompaniesSection = () => {
   return (
     <section className='mt-64 lg:mt-0'>
       <Container id='companies'>
-        <H4 className='text-themeGray/20 text-center'>trusted by...</H4>
+        <H4 className='text-themeGray/20 text-center'>trusted by</H4>
         <div className='w-full flex flex-row gap-8 justify-between items-center lg:gap-0'>
           {CompanyImages.map((image) => (
             <div key={image.url} className='relative'>
-              <span
-                className='block absolute left-0 right-0 bottom-0 top-0 bg-themeBlack 
-              z-[5] mix-blend-color transition-all duration-200 hover:opacity-0'
-              />
               <Image
                 className='lg:w-max'
                 onLoad={(e) => {
