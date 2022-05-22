656c885a1f497897473c05831ee10fee29db55fb
Cesar Gallardo <wuelo789@gmail.com>
2022-05-22 11:52:58 -0500

    Rounded corners

--- src/components/Layout/Modal.tsx
+++ src/components/Layout/Modal.tsx
@@ -23,7 +23,7 @@ export const Modal = ({ isOpen, onClose, children }: Props) => {
       <div
         className='fixed left-0 top-0 bottom-0 right-0 bg-opacity-70 bg-themeBlack z-[500] modal-container
     flex flex-col items-center justify-center'>
-        <Container className='max-h-screen overflow-y-auto overflow-x-hidden bg-light-blue p-8 pop-in'>
+        <Container className='max-h-screen overflow-y-auto overflow-x-hidden bg-light-blue p-8 rounded-md pop-in'>
           <div className='flex flex-row justify-end mb-16'>
             <Button onClick={onClose} className='w-[52px]' theme='error'>
               X
