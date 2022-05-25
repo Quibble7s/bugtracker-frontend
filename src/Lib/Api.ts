6a476f3ece6836399e19b70aa42a8980a4330458
Cesar Gallardo <wuelo789@gmail.com>
2022-05-24 23:56:18 -0500

    Handled errors

--- src/Lib/Api.ts
+++ src/Lib/Api.ts
@@ -12,52 +12,64 @@ export const CreateProject = async (params: {
   description: string;
 }): Promise<{ project: Project | null; message: string; status: number }> => {
   const token = GetToken();
-  const response = await fetch(`${BASE_URL}/api/v1/project/create`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(params),
-  });
-  //Project created successfuly
-  if (response.status === 201) {
-    const project = await response.json();
-    return {
-      project,
-      message: 'Project created successfuly.',
-      status: response.status,
-    };
-  }
-  //User not found
-  if (response.status === 204) {
-    const errorData: ErrorResponse = await response.json();
+  try {
+    const response = await fetch(`${BASE_URL}/api/v1/project/create`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(params),
+    });
+    //Project created successfuly
+    if (response.status === 201) {
+      const project = await response.json();
+      return {
+        project,
+        message: 'Project created successfuly.',
+        status: response.status,
+      };
+    }
+    //User not found
+    if (response.status === 204) {
+      const errorData: ErrorResponse = await response.json();
+      return {
+        project: null,
+        message: errorData.message,
+        status: errorData.status,
+      };
+    }
+    //Any other error
     return {
       project: null,
-      message: errorData.message,
-      status: errorData.status,
+      message: (await response.json()).title,
+      status: response.status,
+    };
+  } catch {
+    return {
+      project: null,
+      message: "Couldn't connect with the server.",
+      status: 500,
     };
   }
-  //Any other error
-  return {
-    project: null,
-    message: (await response.json()).title,
-    status: response.status,
-  };
 };
 
 export const GetProjects = async (): Promise<Project[]> => {
   const token = GetToken();
-  const response = await fetch(`${BASE_URL}/api/v1/project/all`, {
-    method: 'GET',
-    headers: {
-      authorization: `Bearer ${token}`,
-    },
-  });
-  if (response.status === 200) {
-    return await response.json();
+  try {
+    const response = await fetch(`${BASE_URL}/api/v1/project/all`, {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.status === 200) {
+      return await response.json();
+    }
+    return null!;
+  } catch {
+    return null!;
   }
-  return null!;
 };
 
 export const GetProject = async (projectID: string): Promise<Project> => {
@@ -79,22 +91,29 @@ export const JoinProject = async (
   callBack: ({ message, status }: { message: string; status: number }) => void,
 ): Promise<void> => {
   const token = GetToken();
-  const response = await fetch(`${BASE_URL}/api/v1/project/${projectID}/join`, {
-    method: 'PUT',
-    headers: {
-      authorization: `Bearer ${token}`,
-    },
-  });
-  if (response.status === 204) {
-    callBack({ message: 'Successfuly joined project.', status: 204 });
-    return;
-  }
-  if (response.status === 404) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/api/v1/project/${projectID}/join`,
+      {
+        method: 'PUT',
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    if (response.status === 204) {
+      callBack({ message: 'Successfuly joined project.', status: 204 });
+      return;
+    }
+    if (response.status === 404) {
+      const responseData = await response.json();
+      callBack({ message: responseData.message, status: 404 });
+      return;
+    }
+    //If there is a server error or unauthorized
     const responseData = await response.json();
-    callBack({ message: responseData.message, status: 404 });
-    return;
+    callBack({ message: responseData.title, status: response.status });
+  } catch {
+    callBack({ message: "Couldn't reach the server.", status: 500 });
   }
-  //If there is a server error or unauthorized
-  const responseData = await response.json();
-  callBack({ message: responseData.title, status: response.status });
 };
