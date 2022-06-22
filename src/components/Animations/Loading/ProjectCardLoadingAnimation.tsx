79ef2ccd4fb365ba1c46c2f6c7b2b8b4f9d71d0b
Cesar Gallardo <wuelo789@gmail.com>
2022-06-21 23:27:59 -0500

    Updated project card loading animation

--- src/components/Animations/Loading/ProjectCardLoadingAnimation.tsx
+++ src/components/Animations/Loading/ProjectCardLoadingAnimation.tsx
@@ -2,21 +2,13 @@ import './Styles/ProjectCardLoadingAnimation.css';
 
 export const ProjectCardLoadingAnimation = () => {
   return (
-    <div
-      className='w-full p-4 bg-themeLightGray rounded-md border cursor-pointer
-    flex flex-col justify-between min-h-[360px] max-h-[360px] overflow-y-auto'>
-      <div>
-        <div className='w-3/4 mx-auto rounded-md h-[24px] field-loading'></div>
-        <div className='mt-8 rounded-md flex flex-col gap-2'>
-          <div className='w-full h-[16px] rounded-md field-loading' />
-          <div className='w-full h-[16px] rounded-md field-loading' />
-          <div className='w-3/4 h-[16px] rounded-md field-loading' />
-          <div className='w-full h-[16px] rounded-md field-loading' />
+    <div className='w-full min-h-[300px] max-h-[300px] bg-themeLightGray rounded-md overflow-y-auto p-4 cursor-pointer issue-container overflow-x-hidden'>
+      <div className='bg-light-blue w-full h-full rounded-md border-b border-themeGray/25 grid grid-rows-6 p-4 relative overflow-y-auto'>
+        <div className='row-span-4'></div>
+        <div className='row-span-2 flex flex-col justify-between'>
+          <div className='w-3/4 h-[18px] rounded-md field-loading' />
+          <div className='w-2/4 h-[16px] rounded-md field-loading' />
         </div>
-      </div>
-      <div className='grid grid-cols-2'>
-        <div className='w-3/4 h-[16px] rounded-md field-loading' />
-        <div className='w-2/4 ml-auto h-[16px] rounded-md field-loading' />
       </div>
     </div>
   );
