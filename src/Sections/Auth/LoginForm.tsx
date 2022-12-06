86c62f4ad797eebfba57acb1a9f6b01953fcd694
Cesar <wuelo789@gmail.com>
2022-12-06 18:58:08 -0500

    Now the app redirects the user to the dashboard page when login in

--- src/Sections/Auth/LoginForm.tsx
+++ src/Sections/Auth/LoginForm.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { LoadingButton } from 'src/Components/Buttons';
-import { Input } from 'src/Components/Form';
-import { H4, PXS } from 'src/Components/Typography';
-import { useAlert, useAuth } from 'src/Hooks';
-import { AuthForm } from './AuthForm';
+import { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { LoadingButton } from "src/Components/Buttons";
+import { Input } from "src/Components/Form";
+import { H4, PXS } from "src/Components/Typography";
+import { useAlert, useAuth } from "src/Hooks";
+import { AuthForm } from "./AuthForm";
 
 export const LoginForm = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -12,7 +12,7 @@ export const LoginForm = () => {
   const { alert } = useAlert();
   const navigate = useNavigate();
   const locationState: any = useLocation().state;
-  const prevLocation: string = locationState?.from || '/';
+  const prevLocation: string = "/dashboard";
 
   const onSubmitHandler = async (data: any) => {
     const params = { email: data.email, password: data.password };
@@ -20,45 +20,47 @@ export const LoginForm = () => {
     await auth.logIn(params, ({ message, status }) => {
       setIsLoading(false);
       if (status === 200) {
-        alert(message, 'success', 2.5);
+        alert(message, "success", 2.5);
         navigate(prevLocation, { replace: true });
         return;
       }
-      alert(message, 'error', 2.5);
+      alert(message, "error", 2.5);
     });
   };
   return (
     <AuthForm onSubmitHandler={onSubmitHandler}>
-      <H4 className='text-center text-themeGray mb-4'>Login</H4>
+      <H4 className="text-center text-themeGray mb-4">Login</H4>
       <Input
-        id='email'
-        name='email'
-        placeholder='Email'
-        type='email'
+        id="email"
+        name="email"
+        placeholder="Email"
+        type="email"
         requiered
       />
       <Input
-        type='password'
-        id='password'
-        name='password'
-        placeholder='Password'
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Password"
         minLenght={8}
         requiered
       />
-      <PXS className='text-center text-themeGray mix-blend-multiply'>
-        Don't have an account?...{' '}
+      <PXS className="text-center text-themeGray mix-blend-multiply">
+        Don't have an account?...{" "}
         <Link
-          className='underline transition-colors duration-200 hover:text-secondary'
-          to='/auth/register'>
+          className="underline transition-colors duration-200 hover:text-secondary"
+          to="/auth/register"
+        >
           Sign up
         </Link>
         .
       </PXS>
       <LoadingButton
-        type='submit'
-        className='w-fit min-w-[100px] mx-auto'
-        theme='secondary'
-        isLoading={isLoading}>
+        type="submit"
+        className="w-fit min-w-[100px] mx-auto"
+        theme="secondary"
+        isLoading={isLoading}
+      >
         Login
       </LoadingButton>
     </AuthForm>
