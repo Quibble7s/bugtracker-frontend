eb3ca0a0972b570b800fa74c5d77880d390073aa
Cesar <wuelo789@gmail.com>
2022-12-06 18:41:20 -0500

    Updated prod server url

--- src/Constants/Server.ts
+++ src/Constants/Server.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://bugtracker-server-production.up.railway.app/'
+    ? 'https://bugtracker-server-production.up.railway.app'
     : 'https://localhost:44362';
